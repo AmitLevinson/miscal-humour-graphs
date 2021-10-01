ce6e088e2c0f53c5b450e59b8d98a5765dcc271d
AmitLevinson <amitlevinson1@gmail.com>
2021-10-01 14:49:08 +0300

    cleanup plot and code

--- after-the-holidays/xkcdplot.R
+++ after-the-holidays/xkcdplot.R
@@ -1,38 +1,38 @@
 library(ggplot2)
 library(xkcd)
-library(extrafont)
+library(showtext)
+
+font_add("xkcd", "xkcd.ttf")
+showtext_auto()
 
 dat <- data.frame(day=c(1:30),
-                  number=c(seq(1, 80, length.out = 3), seq(100, 200, length.out = 6), seq(200,250,length.out = 7),seq(260,360, length.out = 14)))
+                  number=c(seq(1, 30, length.out = 5), seq(30, 100, length.out = 6), seq(300,450,length.out = 7),seq(450,560, length.out = 12)))
+
 
 xrange <- range(dat$day)
 yrange <- range(dat$number)
 
 
 ggplot(dat, aes(x = day, y = number))+
-  geom_smooth(method = "loess", fill = "transparent")+
+  geom_smooth(method = "loess", fill = "transparent", color = "black")+
   scale_x_continuous(breaks = c(1,30))+
   xkcdaxis(xrange = xrange, yrange = yrange)+
-  coord_cartesian(clip= "off")+
   labs(title = "Jewish Holiday Season",
-       caption = "Visualiztion: Amit Levinson",
+       caption = paste0("Visulaiziton: Amit Levinson"),
        x = "Days into September",
-       # y = "What day is it?",
-       # x = NULL,
        y = NULL
        )+
-  annotate("text", x = 3, y = yrange[2]-20, label = "What day is it?", family = "xkcd", size = 10)+
-  # annotate("text", x = 28, y = 5, label = "Days in september", family = "xkcd", size = 12)+
+  annotate("text", x = 3.5, y = yrange[2], label = "What day is it?", family = "xkcd", size = 17)+
   theme(
-    text = element_text(family = "xkcd"),
     axis.text.y = element_blank(),
-    axis.text.x = element_text(vjust = 2, size = 14),
+    axis.text.x = element_text(vjust = 2, size = 20),
     axis.ticks = element_blank(),
-    axis.title.x = element_text(hjust = 1, size = 22),
-    axis.title.y = element_text(size = 22),
-    plot.caption = element_text(size = 12, hjust = 0, vjust = 3, color = "gray35"),
-    plot.title = element_text(hjust = 0.5, size = 36, face= "bold"),
-    plot.margin = margin(6,8,6,8,"pt")
+    axis.title.x = element_text(hjust = 1, size = 45),
+    plot.caption = element_text(size = 25, hjust = 0, vjust = 3, color = "gray35"),
+    plot.title = element_text(hjust = 0.5, size = 65, face= "bold"),
+    plot.margin = margin(6,8,6,8,"mm"),
+    plot.background = element_rect(fill = "white", color = NA)
   )
 
-ggsave("holiday-plot.png", width = 6, height = 4)
+ggsave("holiday-plot.png", width = 8, height = 5)
+
